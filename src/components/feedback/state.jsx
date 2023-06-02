2f12c55daaef6fb9de810eaeeab3b17d5e9bfcb2
MaksSurzhko <117912210+MaksSurzhko@users.noreply.github.com>
2023-06-02 12:59:33 +0300

    Update state.jsx

--- src/components/feedback/state.jsx
+++ src/components/feedback/state.jsx
@@ -1,62 +1,5 @@
+
 /*import React, { Component } from "react";
-
-class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };
-
-  goodClick = () => {
-      this.setState({
-        good: this.state.good + 1});
-    };
-    
-  neutralClick = () => {
-      this.setState({
-        neutral: this.state.neutral + 1});
-    };
-    
-   badClick = () => {
-      this.setState({
-        bad: this.state.bad + 1});
-    };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-
-    return (
-      <div>
-        <div>
-          <h2>Please leave feedback</h2>
-          <ul>
-            <li>
-              <button type="button" onClick={this.goodClick}>Good</button>
-            </li>
-            <li><button type="button" onClick={this.neutralClick}>Neutral</button></li>
-            <li><button type="button" onClick={this.badClick}>Bad</button></li>
-          </ul>
-        </div>
-        <div>
-          <h2>Statistic</h2>
-          <ul>
-            <li>Good: {good}</li>
-            <li>Neutral: {neutral}</li>
-            <li>Bad: {bad}</li>
-          </ul>
-        </div>
-      </div>
-    );
-  }
-}
-
-export default Feedback;*/
-
-
-
-
-
-import React, { Component } from "react";
 import scss from "./state.module.css"
 
 class Feedback extends Component {
@@ -123,7 +66,7 @@ class Feedback extends Component {
           </ul>
         </div>
         {totalFeed > 0 ? (
-          <div className={scss.blockThree}>
+        <div className={scss.blockThree}>
             <h2 className={scss.titleTwo}>Statistic</h2>
             <ul className={scss.lists}>
               <li className={scss.itemsy}>Good: {good}</li>
@@ -134,19 +77,78 @@ class Feedback extends Component {
             </ul>
           </div>
         ) : (
+            <div>
+            <h2 className={scss.titleTwo}>Statistic</h2>          
            <p className={scss.message}>There is no feedback</p>
+           </div>
         )}
       </div>
     );
   }
 }
 
-export default Feedback;
+export default Feedback;*/
 
 
+import React, { Component } from "react";
+import scss from "./state.module.css";
 
+class Statistics extends Component {
+  render() {
+    const { good, neutral, bad, total, positivePercentage } = this.props;
 
-/*import React, { Component } from "react";
+    return total > 0 ? (
+      <div className={scss.blockThree}>
+        
+        <ul className={scss.lists}>
+          <li className={scss.itemsy}>Good: {good}</li>
+          <li className={scss.items}>Neutral: {neutral}</li>
+          <li className={scss.itemsy}>Bad: {bad}</li>
+          <li className={scss.items}>Total feedback: {total}</li>
+          <li className={scss.itemsy}>
+            Positive feedback percentage: {positivePercentage}%
+          </li>
+        </ul>
+      </div>
+    ) : (
+      <div>
+        
+        <p className={scss.message}>There is no feedback</p>
+      </div>
+    );
+  }
+}
+
+class FeedbackOptions extends Component {
+  render() {
+    const { options, onLeaveFeedback } = this.props;
+
+    return (
+      <ul className={scss.list}>
+        {options.map((option, index) => (
+          <li className={scss.item} key={index}>
+            <button className={scss.btn} type="button" onClick={() => onLeaveFeedback(option)}>
+              {option}
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+class Section extends Component {
+  render() {
+    const { title, children } = this.props;
+
+    return (
+      <div>
+        <h2 className={scss.title}>{title}</h2>
+        {children}
+      </div>
+    );
+  }
+}
 
 class Feedback extends Component {
   state = {
@@ -155,21 +157,9 @@ class Feedback extends Component {
     bad: 0
   };
 
-  goodClick = () => {
-    this.setState(prevState => ({
-      good: prevState.good + 1
-    }));
-  };
-
-  neutralClick = () => {
-    this.setState(prevState => ({
-      neutral: prevState.neutral + 1
-    }));
-  };
-
-  badClick = () => {
-    this.setState(prevState => ({
-      bad: prevState.bad + 1
+  handleFeedback = (type) => {
+    this.setState((prevState) => ({
+      [type]: prevState[type] + 1
     }));
   };
 
@@ -190,108 +180,25 @@ class Feedback extends Component {
     const positivePercentage = this.feedbackPercentage();
 
     return (
-      <div>
-        <div>
-          <h2>Please leave feedback</h2>
-          <ul>
-            <li>
-              <button type="button" onClick={this.goodClick}>
-                Good
-              </button>
-            </li>
-            <li>
-              <button type="button" onClick={this.neutralClick}>
-                Neutral
-              </button>
-            </li>
-            <li>
-              <button type="button" onClick={this.badClick}>
-                Bad
-              </button>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h2>Statistic</h2>
-          <ul>
-            <li>Good: {good}</li>
-            <li>Neutral: {neutral}</li>
-            <li>Bad: {bad}</li>
-            <li>Total feedback: {totalFeed}</li>
-            <li>Positive feedback percentage: {positivePercentage}%</li>
-          </ul>
-        </div>
+      <div className={scss.block}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={["good", "neutral", "bad"]}
+            onLeaveFeedback={this.handleFeedback}
+          />
+        </Section>
+        <Section title="Statistic">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeed}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
       </div>
     );
   }
 }
 
-export default Feedback;*/
-
-
-//ReactDOM.render(<Feedback good={1} />, document.getElementById("root"));
-
-
-
-
-
-
-
-
-
-
-
-
-/*import React, { Component } from "react";
-
-class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };
-
-  btnClick = (type) => {
-    this.setState((prevState) => ({
-      [type]: prevState[type] + 1
-    }));
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <div>
-        <div>
-          <h2>Please leave feedback</h2>
-          <ul>
-            <li>
-              <button onClick={() => this.btnClick("good")}>
-                Good
-              </button>
-            </li>
-            <li>
-              <button onClick={() => this.btnClick("neutral")}>
-                Neutral
-              </button>
-            </li>
-            <li>
-              <button onClick={() => this.btnClick("bad")}>
-                Bad
-              </button>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h2>Statistics</h2>
-          <ul>
-            <li>Good: {good}</li>
-            <li>Neutral: {neutral}</li>
-            <li>Bad: {bad}</li>
-          </ul>
-        </div>
-      </div>
-    );
-  }
-}
-
-export default Feedback;*/
+export default Feedback;
